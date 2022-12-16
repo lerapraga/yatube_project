04437ff3051b81fc8d5319adc367d654461b9284
Lera Praga <abastrenie@mail.ru>
2022-12-16 20:16:03 +0500

    Добавлены urls и view-функции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
-# Create your views here.
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse('Главная страница блога')
+
+
+def group_posts(request, slug):
+    return HttpResponse('Страница замечательных сообществ')
